2d6dab4357f65b0bc5df839be9a7b8a41cfccb81
Prince Lohia <princelohia@Princes-MacBook-Air.local>
2024-12-22 01:28:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/instagramui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.instagramui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+
+@OptIn(ExperimentalFoundationApi::class)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProfileScreen()
+        }
+    }
+}
